d55734a81e659bdb2771d475a6db4fc4c92e8084
radek <radek.glogowski3@gmail.com>
2024-05-14 09:36:33 +0200

    feat: link layout with outlet

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,28 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 // pages
+import AppLayout from "./UI/AppLayout";
 import HomePage from "./Pages/Home/HomePage";
 import GalleryPage from "./Pages/Gallery/GalleryPage";
 import AddFlowerPage from "./Pages/AddFlower/AddFlowerPage";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/add-flower",
-    element: <AddFlowerPage />,
-  },
-  {
-    path: "/gallery",
-    element: <GalleryPage />,
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/add-flower",
+        element: <AddFlowerPage />,
+      },
+      {
+        path: "/gallery",
+        element: <GalleryPage />,
+      },
+    ],
   },
 ]);
 
