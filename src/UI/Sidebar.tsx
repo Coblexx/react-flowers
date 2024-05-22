7ce4b02cf341c53031a091967c6e34cf9d50707f
radek <radek.glogowski3@gmail.com>
2024-05-22 17:05:07 +0200

    feat: refactor nav, styling

--- src/UI/Sidebar.tsx
+++ src/UI/Sidebar.tsx
@@ -1,24 +1,41 @@
-import { NavLink } from "react-router-dom";
+import { ReactNode } from "react";
+import { Link, NavLink } from "react-router-dom";
+
+type NavItemType = {
+  to?: string;
+  children: ReactNode;
+};
 
 export default function Sidebar() {
   return (
     <aside className="bg-slate-300 md:flex md:flex-col hidden min-w-60 text-2xl">
       <nav>
-        <ul className="gap-4">
-          <li>
-            <NavLink to="/home">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/gallery">Gallery</NavLink>
-          </li>
-          <li>
-            <NavLink to="/add-flower">Add Flower</NavLink>
-          </li>
-          <li>
-            <NavLink to="/login">Log in</NavLink>
-          </li>
+        <Logo />
+        <ul className="gap-6">
+          <NavItem to="/home">Home</NavItem>
+          <NavItem to="/gallery">Gallery</NavItem>
+          <NavItem to="/add-flower">Add Flower</NavItem>
+          <NavItem to="/login">Log In</NavItem>
         </ul>
       </nav>
     </aside>
+  );
+}
+
+function NavItem({ to = "/", children }: NavItemType) {
+  return (
+    <li className="p-2 hover:bg-slate-400 pointer">
+      <NavLink className={"w-full block"} to={to}>
+        {children}
+      </NavLink>
+    </li>
+  );
+}
+
+function Logo() {
+  return (
+    <Link to="/" className="flex justify-center py-6 text-2xl">
+      🌸ReactFlowers🌸
+    </Link>
   );
 }
