8ec9b40586ce5d39ae8e9e0d6cdc97241cc5e0d2
radek <radek.glogowski3@gmail.com>
2024-05-22 12:33:58 +0200

    chore: add type and render response

--- src/utils/services/flowerAPI.ts
+++ src/utils/services/flowerAPI.ts
@@ -1,8 +1,16 @@
 import supabase from "./supabase";
 
+export type Flower = {
+  id: number;
+  name: string;
+  desc: string;
+  image: string;
+  primary_color: string;
+};
+
 export async function getFlowers() {
   const { data, error } = await supabase.from("flowers").select("*");
 
   if (error) console.log(error);
-  return data;
+  return data as Flower[];
 }
